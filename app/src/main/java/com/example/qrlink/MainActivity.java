7d7c738f5b37eeaaa56b92bf20ea1f6465fb7139
Cheetack <vgntx91psandroid@gmail.com>
2020-09-13 20:00:15 +0900

    change textview to edittext.

--- app/src/main/java/com/example/qrlink/MainActivity.java
+++ app/src/main/java/com/example/qrlink/MainActivity.java
@@ -6,14 +6,31 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,9 +59,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
 
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        textView.setText(result.getContents());
-
+        EditText body = (EditText)findViewById(R.id.editTextTextPersonName);
+        body.setText(result.getContents(), TextView.BufferType.NORMAL);
     }
 
     // クリップボードにコピー
@@ -74,6 +90,73 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    private void createQRcode() {
+
+        Bitmap qr = null;
+        try {
+            qr = createQRCodeByZxing("http://google.co.jp", 400);
+        } catch (WriterException e) {
+            Log.d("createQRcode", "error: ", e);
+        }
+
+        try {
+            File root = Environment.getExternalStorageDirectory();
+
+            // 日付でファイル名を作成　
+            Date mDate = new Date();
+            SimpleDateFormat fileName = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+            // 保存処理開始
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(new File(root, fileName.format(mDate) + ".jpg"));
+
+            // jpegで保存
+            qr.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
+            // 保存処理終了
+            fos.close();
+        } catch (Exception e) {
+            Log.e("Error", "" + e.toString());
+        }
+    }
+
+    public Bitmap createQRCodeByZxing(String contents,int size) throws WriterException {
+        //QRコードをエンコードするクラス
+        QRCodeWriter writer = new QRCodeWriter();
+
+        //異なる型の値を入れるためgenericは使えない
+        Hashtable encodeHint = new Hashtable();
+
+        //日本語を扱うためにシフトJISを指定
+        encodeHint.put(EncodeHintType.CHARACTER_SET, "shiftjis");
+
+        //エラー修復レベルを指定
+        //L 7%が復元可能
+        //M 15%が復元可能
+        //Q 25%が復元可能
+        //H 30%が復元可能
+        encodeHint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+        BitMatrix qrCodeData = writer.encode(contents, BarcodeFormat.QR_CODE, size, size, encodeHint);
+
+        //QRコードのbitmap画像を作成
+        Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.argb(255, 255, 255, 255)); //いらないかも
+        for (int x = 0; x < qrCodeData.getWidth(); x++) {
+            for (int y = 0; y < qrCodeData.getHeight(); y++) {
+                if (qrCodeData.get(x, y) == true) {
+                    //0はBlack
+                    bitmap.setPixel(x, y, Color.argb(255, 0, 0, 0));
+                } else {
+                    //-1はWhite
+                    bitmap.setPixel(x, y, Color.argb(255, 255, 255, 255));
+                }
+            }
+        }
+
+        return bitmap;
+    }
+
     // 読み取りボタン
     public void scanBarcode(View view) {
         IntentIntegrator integrator = new IntentIntegrator(this);
@@ -86,12 +169,13 @@ public class MainActivity extends AppCompatActivity {
 
     // コピーボタン
     public void copyButtonClick(View view) {
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        copyToClipBoard(textView.getText().toString());
+        EditText body = (EditText)findViewById(R.id.editTextTextPersonName);
+        copyToClipBoard(body.getText().toString());
     }
 
+    // ブラウザで開くボタン
     public void openUrlButtonClick(View view) {
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        openUrlByText(textView.getText().toString());
+        EditText body = (EditText)findViewById(R.id.editTextTextPersonName);
+        openUrlByText(body.getText().toString());
     }
 }
